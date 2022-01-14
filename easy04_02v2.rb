cb48ca9d99d53814a5564ff2161b2a65c42da50f
apopheny <cedouglass@gmail.com>
2022-01-13 20:18:50 -0500

    not sure why I have such problems with easy 4-2

---
+++ easy04_02v2.rb
@@ -0,0 +1,63 @@
+
+# What Century is That?
+
+# Write a method that takes a year as input and returns the century. The return value should be a string that begins with the century number, and ends with st, nd, rd, or th as appropriate for that number.
+
+# New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
+
+# Examples:
+
+# century(2000) == '20th'
+# century(2001) == '21st'
+# century(1965) == '20th'
+# century(256) == '3rd'
+# century(5) == '1st'
+# century(10103) == '102nd'
+# century(1052) == '11th'
+# century(1127) == '12th'
+# century(11201) == '113th'
+
+WORK IN PROGRESS
+def special_cases(int)
+  outliers = [11, 12, 13]
+  outliers.include?(int)
+end
+
+def century(int)
+
+end
+
+1st 
+2nd 
+3rd 
+4th
+5th
+6th
+7th
+8th
+9th
+10th
+
+## 11th
+## 12th
+## 13th
+14th
+15th
+16th
+17th
+18th
+19th
+20th
+
+
+
+
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
